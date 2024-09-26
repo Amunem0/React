c2776f05cf2c44b8d6c155f64df1fdae6ebdef14
Amunem0 <aryankashikar7@gmail.com>
2024-09-27 00:20:56 +0530

    first commit

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
     <h1>Username={username}</h1>
-    <h1>Chai au react with vite|Aryan</h1> 
+    <h1>Chai and react with vite|Aryan</h1> 
     <Chai/>
     </>
     
